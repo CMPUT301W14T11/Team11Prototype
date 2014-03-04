8ff99b3fb6c1e9f2e40bb2c68936515d002d203d
coolguy101 <jiang_501@hotmail.com>
2014-03-04 14:06:48 -0700

    did a example in subcomment

--- LocationApp/src/com/example1/locationapp/SubCommetsRead.java
+++ LocationApp/src/com/example1/locationapp/SubCommetsRead.java
@@ -1,5 +1,9 @@
 package com.example1.locationapp;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import Model.Comments;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -11,21 +15,31 @@ public class SubCommetsRead extends Activity {
 	private ListView listViewSubComment;
 	private EditText editText;
 	private Button button1;
+	private cutadapter ListAdapter;
+	ArrayList<Comments> comment_list; 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_sub_commets_read);
-		listViewSubComment = (ListView)findViewById(R.id.listViewSubComments);
-		editText = (EditText)findViewById(R.id.editTextSubmitSubComments);
-		button1 = (Button)findViewById(R.id.buttonSaveSubComments);
-		button1.setText("Submit Sub Comments");
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_sub_commets_read);
+	listViewSubComment = (ListView)findViewById(R.id.listViewSubComments);
+	editText = (EditText)findViewById(R.id.editTextSubmitSubComments);
+	button1 = (Button)findViewById(R.id.buttonSaveSubComments);
+	button1.setText("Submit Sub Comments");
+    comment_list = new ArrayList<Comments>();
+    // add an example to test the list
+    comment_list.add(new Comments(0, 0, "It works", "Tesing", new Date(), null, 123, 123, null));
+	ListAdapter = new cutadapter(this, R.layout.listlayout, comment_list);
+	
+	listViewSubComment.setAdapter(ListAdapter);
+	
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.sub_commets_read, menu);
-		return true;
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.sub_commets_read, menu);
+	return true;
 	}
 
 }
+
