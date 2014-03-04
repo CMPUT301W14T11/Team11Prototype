ce4c0ef08df55d697dbafcae419d7a01ff371cf7
chongyangye <cye2@ualberta.ca>
2014-03-03 21:12:28 -0700

    subComments interface created

--- LocationApp/src/com/example1/locationapp/SubCommetsRead.java
+++ LocationApp/src/com/example1/locationapp/SubCommetsRead.java
@@ -3,13 +3,22 @@ package com.example1.locationapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class SubCommetsRead extends Activity {
-
+	private ListView listViewSubComment;
+	private EditText editText;
+	private Button button1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sub_commets_read);
+		listViewSubComment = (ListView)findViewById(R.id.listViewSubComments);
+		editText = (EditText)findViewById(R.id.editTextSubmitSubComments);
+		button1 = (Button)findViewById(R.id.buttonSaveSubComments);
+		button1.setText("Submit Sub Comments");
 	}
 
 	@Override
