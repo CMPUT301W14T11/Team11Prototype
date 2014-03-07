b2a4d060c03347319fd267a56e9bf8f6869b0156
jasonqi77 <bqi@ualberta.ca>
2014-03-07 12:15:03 -0700

    add faviourte model

---
+++ LocationApp/src/Model/Faviourte.java
@@ -0,0 +1,38 @@
+package Model;
+
+
+
+public class Faviourte
+{
+	private String username;
+	private Comments faviourte;
+	
+	public String getUsername()
+	{
+		
+		return username;
+	}
+	
+	public void setUsername(String username)
+	{
+	
+		this.username = username;
+	}
+
+	
+	public Comments getFaviourte()
+	{
+	
+		return faviourte;
+	}
+
+	
+	public void setFaviourte(Comments faviourte)
+	{
+	
+		this.faviourte = faviourte;
+	}
+	
+
+	
+}
