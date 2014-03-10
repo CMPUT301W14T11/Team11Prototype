35af93ec098c5a25afce94b0696bc81c6c877623
jasonqi77 <bqi@ualberta.ca>
2014-03-10 15:26:24 -0600

    file loader and saver

---
+++ LocationApp/src/Controller/LocalFileLoder.java
@@ -0,0 +1,53 @@
+package Controller;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import Model.UserModel;
+import android.content.Context;
+
+import com.google.gson.Gson;
+
+
+public class LocalFileLoder
+{
+
+	private static final String FILENAME = "fav1.sav";
+	/**
+	 * @uml.property  name="counter"
+	 * @uml.associationEnd  
+	 */
+
+	private Gson gson = new Gson();
+	private Context context;
+	private UserModel um;
+	
+	public LocalFileLoder(Context context)
+	{
+		this.context = context;
+	}
+	
+	public UserModel loadFromFile() {      
+		um = new UserModel();
+        try {
+                FileInputStream fis = context.openFileInput(FILENAME);
+                BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+                String line = in.readLine();
+                while (line != null) {
+                	um = gson.fromJson(line, UserModel.class);                       
+                        line = in.readLine();
+                }
+
+        } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+        } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+        }
+        return um;
+	}	
+}
