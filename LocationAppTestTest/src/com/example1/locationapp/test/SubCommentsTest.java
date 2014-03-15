4b71b2f7db70b842b63a6047cf260308b14b6b07
chongyangye <cye2@ualberta.ca>
2014-03-15 11:00:32 -0600

    subcomment

---
+++ LocationAppTestTest/src/com/example1/locationapp/test/SubCommentsTest.java
@@ -0,0 +1,22 @@
+package com.example1.locationapp.test;
+
+import Controller.SubCommentController;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.example1.locationapp.SubCommetsRead;
+
+public class SubCommentsTest extends ActivityInstrumentationTestCase2<SubCommetsRead> {
+
+	private SubCommetsRead subComments;
+	private SubCommentController subCont;
+	public SubCommentsTest() {
+		super(SubCommetsRead.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	public void subGetCommentsTest(){
+		
+	}
+}
