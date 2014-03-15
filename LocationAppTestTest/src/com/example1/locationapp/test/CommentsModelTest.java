cc1311e1a2e62d2c245135ed57d021a56d1e3c56
coolguy101 <jiang_501@hotmail.com>
2014-03-15 09:58:16 -0600

    test

--- LocationAppTestTest/src/com/example1/locationapp/test/CommentsModelTest.java
+++ LocationAppTestTest/src/com/example1/locationapp/test/CommentsModelTest.java
@@ -1,11 +1,22 @@
 package com.example1.locationapp.test;
 
+import com.example1.locationapp.MainActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.AndroidTestCase;
 import Model.Comments;
 import junit.framework.TestCase;
 
-public class CommentsModelTest extends TestCase {
+public class CommentsModelTest extends ActivityInstrumentationTestCase2<MainActivity>{
 
 	
+	
+
+	public CommentsModelTest(Class<MainActivity> activityClass) {
+		super(activityClass);
+		// TODO Auto-generated constructor stub
+	}
+
 	Comments comments ;
 	protected void setUp() throws Exception {
 		super.setUp();
