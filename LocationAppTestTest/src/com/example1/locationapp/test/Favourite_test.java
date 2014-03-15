63e5a9b7d8aec8abfb87e16aa7782125eb7e039f
Yu Zuo <zuo2@cs.ualberta.ca>
2014-03-15 13:30:55 -0600

    testcase of Favourite finished

--- LocationAppTestTest/src/com/example1/locationapp/test/Favourite_test.java
+++ LocationAppTestTest/src/com/example1/locationapp/test/Favourite_test.java
@@ -1,11 +1,14 @@
 package com.example1.locationapp.test;
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.example1.locationapp.Favourite;
+import com.example1.locationapp.GPSTracker;
 
 
 import Model.Comments;
 import Model.FavouriteModel;
+import android.location.Location;
 import android.test.ActivityInstrumentationTestCase2;
 import junit.framework.TestCase;
 
@@ -15,9 +18,23 @@ import junit.framework.TestCase;
 public class Favourite_test extends ActivityInstrumentationTestCase2<Favourite>
 {
 	private String username ="User_test_name";
-	Comments comment = null;
+	private Comments comment = null;
 	ArrayList<Comments> subcomment = null;
+	
+	private int user_id1 = 111;
+	private int masterid1 = 112;
+	private int sid1 = 114;
+	private int subid1 = 113;
+	private String title1 = "gggg";
+	private String subject1 = "ggggjob";
+	private Location location1 = null;
+	
+	private Date the_date1 = new Date(System.currentTimeMillis());
 
+	private double lon1 = 12.3322;
+	private double lat1 = 12.3327;
+	private String encode1 = "sssssss";
+	
 	public Favourite_test()
 	{
 
@@ -30,17 +47,42 @@ public class Favourite_test extends ActivityInstrumentationTestCase2<Favourite>
 	    assertEquals("the output should equal to User_test_name", "User_test_name",fa.getUsername());
 	}
 	
-	public void test_getuserComment(){
+	public void test_getComment(){
 		FavouriteModel fa = new FavouriteModel(username, comment, subcomment);
 	    assertEquals("the output should equal to User_test_name", null,fa.getComment());
 	}
 	
-	public void test_getuserSubComment(){
+	public void test_diff_comments(){
+		Comments test = new Comments(user_id1, masterid1, sid1, subid1, title1, subject1, the_date1, location1, lon1, lat1);
+		Comments test1 =  new Comments(user_id1, masterid1, sid1, subid1, title1, subject1, the_date1, location1, lon1, lat1, encode1);
+		
+		
+		FavouriteModel fa = new FavouriteModel(username, test, subcomment);
+		FavouriteModel fai = new FavouriteModel(username, test1, subcomment);
+		assertNotSame("fai should not equal to fa", fa.getComment(),fai.getComment());
+		
+	}
+	
+	
+	
+	public void test_getSubComment_null(){
 		FavouriteModel fa = new FavouriteModel(username, comment, subcomment);
 	    assertEquals("the output should equal to User_test_name", null,fa.getSubComment());
 	}
 	
-
+	public void test_subComment_which_is_not_null(){
+		Comments test = new Comments(user_id1, masterid1, sid1, subid1, title1, subject1, the_date1, location1, lon1, lat1);
+		Comments test1 =  new Comments(user_id1, masterid1, sid1, subid1, title1, subject1, the_date1, location1, lon1, lat1, encode1);
+		ArrayList<Comments> test_list = new ArrayList<Comments>();
+	    
+	    test_list.add(test);
+	    test_list.add(test1);
+	    
+		
+		FavouriteModel fa = new FavouriteModel(username, comment, test_list);
+		assertEquals("fai should not equal to fa", test_list,fa.getSubComment());
+		
+	}
 
 	protected void setUp() throws Exception
 	{
