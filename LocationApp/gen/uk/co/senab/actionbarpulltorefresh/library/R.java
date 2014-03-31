3740c7d2211c698dd307f3f2ff2374b8c4339828
jasonqi77 <bqi@ualberta.ca>
2014-03-30 18:11:18 -0600

    subFavourite with bugs

--- LocationApp/gen/uk/co/senab/actionbarpulltorefresh/library/R.java
+++ LocationApp/gen/uk/co/senab/actionbarpulltorefresh/library/R.java
@@ -59,7 +59,7 @@ public final class R {
 		public static final int spb_default_sections_count = 0x7f080000;
 	}
 	public static final class layout {
-		public static final int default_header = 0x7f03000e;
+		public static final int default_header = 0x7f03000f;
 	}
 	public static final class string {
 		public static final int pull_to_refresh_pull_label = 0x7f090001;
