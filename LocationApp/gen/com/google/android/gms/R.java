36bd6a532e9033b98ae217d465108296e8c4dc33
coolguy101 <jiang_501@hotmail.com>
2014-07-05 19:58:28 -0600

    change UI

--- LocationApp/gen/com/google/android/gms/R.java
+++ LocationApp/gen/com/google/android/gms/R.java
@@ -65,10 +65,10 @@ public final class R {
 		public static final int common_signin_btn_text_normal_light = 0x7f020061;
 		public static final int common_signin_btn_text_pressed_dark = 0x7f020062;
 		public static final int common_signin_btn_text_pressed_light = 0x7f020063;
-		public static final int ic_plusone_medium_off_client = 0x7f020086;
-		public static final int ic_plusone_small_off_client = 0x7f020087;
-		public static final int ic_plusone_standard_off_client = 0x7f020088;
-		public static final int ic_plusone_tall_off_client = 0x7f020089;
+		public static final int ic_plusone_medium_off_client = 0x7f020087;
+		public static final int ic_plusone_small_off_client = 0x7f020088;
+		public static final int ic_plusone_standard_off_client = 0x7f020089;
+		public static final int ic_plusone_tall_off_client = 0x7f02008a;
 	}
 	public static final class id {
 		public static final int hybrid = 0x7f05000d;
